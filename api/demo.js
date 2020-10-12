f6079e584b56bb44d7bc4b493ea89efd7403d78c
Khánh Hoàng <hoangchaukhanh93@gmail.com>
2020-10-12 12:56:05 +0700

    feat: add demo api

---
+++ api/demo.js
@@ -0,0 +1,5 @@
+const data = {"id":2,"name":"Demo","description":"","metadata":null,"is_active":true,"created_at":"2020-08-16T11:22:06.000Z","updated_at":"2020-08-16T11:22:06.000Z","modelName":"Company"}
+
+module.exports = (req, res) => {
+  res.json(data)
+}
